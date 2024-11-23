2d1ee7615dd9781759b288db882a7b3de8f68d8c
HienLe20044 <hien.lengoc2004@hcmut.edu.vn>
2024-11-23 18:49:23 +0700

    Update self-hosted

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"log"
+
+	_ "github.com/HienLe2004/coin-price-be-go-demo/docs"
 	"github.com/HienLe2004/coin-price-be-go-demo/routes"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -10,12 +15,22 @@ func init() {
 	//Connect db
 }
 
+// @title Demo Documenting API
+// @version 1
+// @Description coin price go backend
+// @contact.name Ngoc Hien
+// @contact.url https://github.com/HienLe2004/coin-price-be-go-demo
+
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 
 	server := gin.Default()
-
+	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	routes.RegisterRoutes(server)
-
-	server.Run(":8080")
+	err := server.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 	//Set up router, routs, server, websocket
 }
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -6,6 +6,6 @@ import (
 
 func RegisterRoutes(server *gin.Engine) {
 	authenticated := server.Group("/api")
-	authenticated.GET("/v1/funding-rate", getFundingRate)
-	authenticated.GET("/v1/vip1/kline", getKline)
+	authenticated.GET("/v1/funding-rate", GetFundingRate)
+	authenticated.GET("/v1/vip1/kline", GetKline)
 }
